721e95dbfd1ee3ef966c4bc2953df1790a177e94
nick-santos <66573166+nick-santos@users.noreply.github.com>
2026-03-19 16:15:55 -0300

    Limit distance a projectile can reach

--- Assets/Scripts/Shooting/ProjectileBehavior.cs
+++ Assets/Scripts/Shooting/ProjectileBehavior.cs
@@ -8,23 +8,33 @@ public class ProjectileBehavior : MonoBehaviour
     [Header("Attributes")]
     public float speed = 10.0f;
     public int projectileDamage = 1;
+    public float range = 5f;
 
     private Rigidbody2D rb;
     private Transform target;
+    private Vector3 origin;
+    private float distance;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        origin = transform.position;
     }
 
     public void SetTarget (Transform _target) {
         target = _target;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        distance = Vector2.Distance(transform.position, origin);
+        if (Vector2.Distance(transform.position, origin) >= range)
+        {
+            // Debug.Log(Vector2.Distance(transform.position, origin));
+            Destroy(this.gameObject);
+        }
+
         if (!target) return;
 
         Vector2 direction = (target.position - transform.position).normalized;
