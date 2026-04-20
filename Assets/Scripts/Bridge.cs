441855754fe2a5ddccebae3aec4e93e0ecdf2cb5
nick-santos <66573166+nick-santos@users.noreply.github.com>
2026-04-20 19:56:47 -0300

    Add Bridge script

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Bridge : MonoBehaviour
+{
+    public ButtonObject button;
+    private bool active = false;
+
+    private SpriteRenderer sprite;
+    private Collider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (button != null)
+        {
+            button.OnButtonPressed += Event_OnButtonPressed;
+        }
+        sprite = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void Event_OnButtonPressed(object sender, EventArgs e)
+    {
+        active = !active;
+        Debug.Log("The bridge is " + active);
+        sprite.enabled = active;
+        col.enabled = !active;
+    }
+}
