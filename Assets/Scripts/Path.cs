451245dd3814b43fa5c4bb3811ace901329f9def
nick-santos <66573166+nick-santos@users.noreply.github.com>
2026-04-20 20:20:30 -0300

    Add Path script

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Path : MonoBehaviour
+{
+    public ButtonObject button;
+    private bool active = false;
+
+    public float timeUntillDisabled = 10f;
+    private Coroutine pathActivationCoroutine;
+
+    private SpriteRenderer sprite;
+    private Collider2D col;
+
+    void Start()
+    {
+        if (button != null)
+        {
+            button.OnButtonPressed += Event_OnButtonPressed;
+        }
+        sprite = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void Event_OnButtonPressed(object sender, EventArgs e)
+    {
+        if (pathActivationCoroutine != null)
+        {
+            StopCoroutine(pathActivationCoroutine);
+        }
+        pathActivationCoroutine = StartCoroutine(PathActivatedTime());
+    }
+
+    private IEnumerator PathActivatedTime()
+    {
+        EnablePath();
+        yield return new WaitForSeconds(timeUntillDisabled);
+        DisablePath();
+    }
+
+    void EnablePath()
+    {
+        active = true;
+        sprite.enabled = active;
+        col.enabled = !active;
+        Debug.Log("The path is " + active);
+    }
+
+    void DisablePath()
+    {
+        active = false;
+        sprite.enabled = active;
+        col.enabled = !active;
+        Debug.Log("The path is " + active);
+    }
+}
