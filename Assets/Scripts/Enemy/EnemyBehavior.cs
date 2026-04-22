e3c249350502402c72f0988b33b8c9ba8d4788b9
nick-santos <66573166+nick-santos@users.noreply.github.com>
2026-04-22 01:56:07 -0300

    Update EnemyBehavior script
    + Add Wave enemy prefab
    + Update EnemySenses script

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -8,28 +8,55 @@ public class EnemyBehavior : MonoBehaviour
     public GameObject spawnPoint;
 
     [Header("Attributes")]
+    public EnemyMode mode;
     public float speed = 2f;
     public float direction = 1f;
     public bool isSeeing = false; 
     public bool isClose = false;
 
     public Transform targetTransform;
-    Rigidbody2D rb;
+
+    private Rigidbody2D rb;
+    GameObject baseObject;
+    private string baseTag = "Base";
+
+    public enum EnemyMode
+    {
+        Normal,
+        Wave
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        baseObject = GameObject.FindGameObjectWithTag(baseTag);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isSeeing)
+        switch (mode)
+        {
+            case EnemyMode.Normal:
+                NormalBehavior();
+                break;
+
+            case EnemyMode.Wave:
+                WaveBehavior();
+                break;
+        }
+
+        
+    }
+
+    void NormalBehavior()
+    {
+        if (isSeeing)
         {
             Chase();
         }
-        else if(isClose)
+        else if (isClose)
         {
             //Attack();
         }
@@ -37,6 +64,15 @@ public class EnemyBehavior : MonoBehaviour
         {
             //Idle();
         }
+    }
+
+    void WaveBehavior()
+    {
+        if (!isSeeing)
+        {
+            targetTransform = baseObject.transform;
+        }
+        Chase();
     }
 
     void Chase()
--- Assets/Scripts/Enemy/EnemySenses.cs
+++ Assets/Scripts/Enemy/EnemySenses.cs
@@ -43,7 +43,7 @@ public class EnemySenses : MonoBehaviour
         {
             targetsInRange.Add(collision.transform);
 
-            if (parent.targetTransform == null)
+            if (parent.isSeeing == false)
             {
                 parent.targetTransform = collision.transform;
                 parent.isSeeing = true;
